82d1d730a17df2b5b766e84ea95a473e184dd217
Nihant Gs - n0g02cv <Nihant.Gs@walmartlabs.com>
2020-07-06 00:47:57 +0530

    Added code to process kafka post messages

---
+++ src/main/java/com/loader/kafkaloader/KafkaLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.loader.kafkaloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loader/kafkaloader/KafkaLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loader.kafkaloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
