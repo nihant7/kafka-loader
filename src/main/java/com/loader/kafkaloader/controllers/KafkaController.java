9995e16ed81cc5fded280d1630a80821e1064ef4
Nihant <Nihant.Gs@walmartlabs.com>
2020-07-06 00:57:44 +0530

    Added code to process kafka post messages

--- src/main/java/com/loader/kafkaloader/controllers/KafkaController.java
+++ src/main/java/com/loader/kafkaloader/controllers/KafkaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/kafka")
+//Controller
 public class KafkaController {
     private final Producer producer;
     @Autowired
